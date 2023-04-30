59c2fad82e2fa4741bfc593c8c11c71d575fdd35
Aykut Mayali <aykutmayali@gmail.com>
2023-04-30 23:25:03 +0300

    categories added

---
+++ TravelApp/src/components/Categories/style.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    item: {
+        fontSize: 12,
+        color: "rgba(0,0,0,0.5)",
+        marginHorizontal:15,
+        paddingTop: 15,
+        paddingVertical:2,
+    }, 
+    selectedItem :{
+        textDecorationLine: 'underline',
+        color: '#000000'
+    },
+    itemContainer: {
+        paddingVertical: 14,
+        marginRight: 17,
+    },
+    selectedItemContainer: {
+        borderBottomColor: '#4681A3',
+        borderBottomWidth: 1,
+    }
+});
+
+export default styles
--- TravelApp/src/screens/Home/index.js
+++ TravelApp/src/screens/Home/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { SafeAreaView, View } from "react-native";
 import Title from '../../components/Title';
 import styles from './styles';
+import Categories from "../../components/Categories";
 
 const Home= () => {
     return (
@@ -10,6 +11,7 @@ const Home= () => {
                 <Title text="Where do 🤨" style ={{fontWeight: 'normal'}}/>
                 <Title text="you want to go"/>
                 <Title text="Explore Attractions" style={styles.subtitle} />
+                <Categories selectedCategory= 'All' categories={['All', 'Popular', 'Historical', 'Random', 'Trending', 'Exckusive']}/>
             </View>
         </SafeAreaView>
     );
